4b2dc10f8db4ff1659f72a162458b30e3dd2ae5f
adriancorro <adriancorrog@gmail.com>
2021-05-06 10:16:18 +0200

    mejoras en la UI

--- js/main.js
+++ js/main.js
@@ -5,35 +5,49 @@
 
     let addFavoritos = []
     let countries = [];
-    const citiesPrincipal = [];
+    let citiesPrincipal = [];
     let addFav = true
-    let ciudadString ;
     let geolatitude;
     let geolongitude; 
-    let countrieShow;
     
-    const getWeath = (searchlat, searchlon) =>  {
-      // Request API. Las API nos pide que le enviemos 3 parametros 
-    const url = new URL(`https://api.weatherbit.io/v2.0/current`)
+    // Si a getWeath le enviamos como paramtros Lat y Log Hara algo
+    // Si a getWeath le enviamos como paramtros searchCity.value Hara algo
+    // for of citiesPrincipal necesita Promise. De esta forma la API se comporta correctamente, ya que
+    // solo con foreach dara unos errores inesperados.
 
+    const getWeath = (searchlat, searchlon) => new Promise(function(resolve, reject) {
+    //  setTimeout(() => {
+        
+      
+      // Request API. Las API nos pide que le enviemos 3 parametros 
+      const url = new URL(`https://api.weatherbit.io/v2.0/current`)
+      // Hay dos formas de interactuar con el Request: 1) Enviando city 2) Enviando lat o lon
     if(searchCity.value ){
       url.searchParams.set(`city`,searchCity.value)
     }else if (searchlat && searchlon ) {
       // si se esta llamando a getWeath enviando parametros
       url.searchParams.set(`lat`,searchlat)
       url.searchParams.set(`lon`,searchlon)
-
-    } else {
-      url.searchParams.set(`lat`,geolatitude)
-      url.searchParams.set(`lon`,geolongitude)
-    }
+    } 
     url.searchParams.set(`key`,`9fd19cddd4594041b751dcb8f6c01405`)
     url.searchParams.set(`lang`,`es`)
 
     fetch(url)
     // convertimos a json lo que obtenemos
-    .then(response => response.json())
+    .then(response => {
+        if(!response.ok) {
+          console.log('Respuesta de red OK pero respuesta HTTP no OK  ');
+          console.log(response.status);
+          const divChild = document.createElement(`div`)
+          divChild.setAttribute("id", "divChild")
+          //child div
+          divParent.appendChild(divChild);
+          divChild.setAttribute("class", "divChild  error");
+          divChild.innerText = `Network response OK but HTTP response not OK Status: ${response.status}`
+        } return response.json()
+    } )
     .then(json => {
+  
         // datos a obtener
         const weather = json.data[0]
         const icon =  weather.weather.icon
@@ -60,8 +74,8 @@
         addFavoritos.push(weather.lon)
         
         
-       const fetchGetNameCountrieAsyn = async () => {
-         try {
+        const fetchGetNameCountrieAsyn = async () => {
+          try {
               const response = await fetch(`https://restcountries.eu/rest/v2/alpha/${country_code}`);
             if (!response.ok) {
               const message = `An error has occured: ${response.status}`;
@@ -75,62 +89,67 @@
         } catch(e) {
           console.log(e); // 30
         }
-       }
-       fetchGetNameCountrieAsyn()
-/*          Cunado hacemos click a los botones estamos enviando 2 parametros a esta funcion 
+        }
+        fetchGetNameCountrieAsyn()
+  /*          Cuando hacemos click a los botones estamos enviando 2 parametros a esta funcion 
             getWeath(Parametro1, Parametro2) por lo que renderiza segun lat y lon
-            solo si enviamos lat y lon se activara esta parte del codigo
- */        if(searchlat && searchlon && addFav ){
-           //parent div
-          const divChild = document.createElement(`div`)
-          divChild.setAttribute("id", "divChild")
+            solo si enviamos lat y lon como parametros getWeath(parametro1, parametro2)se activara esta parte del codigo
+  */       if(searchlat && searchlon && addFav ){
+          
+            //parent div
+            const divChild = document.createElement(`div`)
+            divChild.setAttribute("id", "divChild")
 
-          //child div
-          divParent.appendChild(divChild);
-          if(searchlat == geolatitude ){
+            //child div
+            divParent.appendChild(divChild);
+            if(searchlat == geolatitude ){
             divChild.setAttribute("class", "divChild principal");
             divChild.innerText = city + " (Current City)"
-          }else{
+            }else{
             divChild.setAttribute("class", "divChild");
-           divChild.innerText = city
-          }
-      
-          countries.push(city)
-         
-          fetchGetNameCountrieAsyn().then(response => {
+            divChild.innerText = city
+            }
+
+            countries.push(city)
+
+            fetchGetNameCountrieAsyn().then(response => {
             const countryDiv = document.createElement(`div`)
             countryDiv.innerText = response.nativeName
             divChild.appendChild(countryDiv);
-            
-          }); 
-          
-          fetchGetNameCountrieAsyn().catch(error => {
+
+            }); 
+
+            fetchGetNameCountrieAsyn().catch(error => {
             error.message; 
             console.log(error.message)
-          });
-          fetchGetNameCountrieAsyn()
-          const imgFav = document.createElement(`img`)
-          imgFav.setAttribute("class", "imgFav");
-          imgFav.src = urlIcon
-          divChild.appendChild(imgFav);
+            });
+            fetchGetNameCountrieAsyn()
+            const imgFav = document.createElement(`img`)
+            imgFav.setAttribute("class", "imgFav");
+            imgFav.src = urlIcon
+            divChild.appendChild(imgFav);
 
-          const temp = document.createElement(`div`)
-          temp.innerText = temperature
-          divChild.appendChild(temp);
+            const temp = document.createElement(`div`)
+            temp.innerText = temperature
+            divChild.appendChild(temp);
 
-          const desc = document.createElement(`div`)
-          desc.innerText = description
-          divChild.appendChild(desc);
-          mapBox()
-        }      
+            const desc = document.createElement(`div`)
+            desc.innerText = description
+            divChild.appendChild(desc);
+            mapBox()
+            }
     })
     .catch((error) => {
+   
         console.log(error)
     }) 
-}
+    resolve(console.log("ok: for (const element of citiesPrincipal) "));
+    //  reject(console.log("Fail: for (const element of citiesPrincipal) ")); 
 
 
- 
+  //  }, 3000);
+    });
+    
 let body = document.querySelector("body")
 let divParent = document.createElement(`div`)
 body.appendChild(divParent);
@@ -161,87 +180,87 @@ btnSearch.addEventListener("click", function( event ) {
     addFavoritos = []
   }, false);
 
+
+
   /* 
-  Queremos que el push de citiesPrincipal sea el de geolocation, pero geolocation tarda unos milisegundos
-  en GET Latitude y Longitide, debido a eso el push no se hace en el orden requerido por eso las siguientes lineas
-  de codigos. Queremos que await geolocation.
+asíncrona funciones
+Queremos que citiesPrincipal[0] sea  geo Location Actual
+podemos cambiar esto dentro asyncF() 
   */
 
-  // Queremos hacer los push por orden ya que geolocation tarda unos milessegunddos mas en reponder
-  // asyncF se encarga de resolver ese problema
-  // si queremos que geolocation este de primero:
-
-  let  geoLocation = () => { 
+  let  geoLocationActualAndPush = () => { 
     return new Promise (resolve => { 
-      window.navigator.geolocation.getCurrentPosition(  geoResult => {
-        // Coordenas obtenidas
-        resolve (
-          geolatitude = geoResult.coords.latitude,
-          geolongitude = geoResult.coords.longitude,)
-      })
+        window.navigator.geolocation.getCurrentPosition(  geoResult => {
+          // Coordenas obtenidas
+            resolve (
+              citiesPrincipal.push({
+                latitude: geoResult.coords.latitude,
+                longitude: geoResult.coords.longitude               
+              }),      
+              geolatitude =  geoResult.coords.latitude,
+              geolongitude = geoResult.coords.longitude,
+              console.log("resolve 1")
+            )
+        })
     });
   } 
 
-  let pushOne = () => {
-    return new Promise(resolve => {
+let pushCitiesFav = () => {
+  return new Promise(resolve => {
       resolve(
         citiesPrincipal.push({
-          latitude: geolatitude,
-          longitude: geolongitude
-        })
+          latitude: 40.41,
+          longitude: -3.70,
+        }),
+    
+        citiesPrincipal.push({
+          latitude: 41.41,
+          longitude: 2.19,
+        }), 
+        console.log("resolve 2"),
+        console.log(citiesPrincipal)
       )
-    })
-   }
-
- 
-
-let pushCitites = () => {
-  return new Promise(resolve => {
-    resolve(
-      
-      citiesPrincipal.push({
-        latitude: 40.41,
-        longitude: -3.70,
-      }),
-  
-      citiesPrincipal.push({
-        latitude: 41.41,
-        longitude: 2.19,
-      })
-    )
   })
-  }
-
- let citiesForeach = () => {
-  return new Promise(resolve => {
-    resolve(
-      citiesPrincipal.forEach( citie =>
-      getWeath(citie.latitude, citie.longitude)) ,
-    )
-  })
- }
-
-
-// El orden para mostrar:
-// De esta forma geo location actual se mostrara de ultimo  let a = await pushOne();
- async function asyncF() {
-  let x = await geoLocation();
-  let y =  await pushCitites();
-  let a = await pushOne();
-  let z = await citiesForeach();
-
 }
 
-asyncF(); 
+// El orden para mostrar:
+// Puede colocar await pushCitiesFav() antes de await pushLocationActual() o de formas deiferentes
+  async function asyncF() {
+  await pushCitiesFav();
+  await geoLocationActualAndPush();
+ 
+      // https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
+      // Usar forEach y await puede dar errores inesperados
+      //forEach() intenta ejecutarse secuencialmente y es posible que no siempre obtenga el resultado esperado.
+      // el siguiente codigo dara error:
+      //citiesPrincipal.forEach( citie => await getWeath(citie.latitude,citie.longitude ))
+  
+      //Para ejecutar todas las promesas de forma secuencial, puede utilizar for ...of
+     /*  for (const element of citiesPrincipal) {
+        await getWeath(element.latitude,element.longitude )
+      } */
 
+     //El siguiente codigo no da error pero es una forma antigua 
+    /*   for (let i = 0; i < citiesPrincipal.length; i++) {
+         await getWeath(citiesPrincipal[i].latitude, citiesPrincipal[i].longitude  ) ;
+      } */
+     
+  for (const element of citiesPrincipal) {
+    await getWeath(element.latitude,element.longitude )
+    console.log( `resolve 3. Element:  ${citiesPrincipal.indexOf(element) }` )
+  }
+ 
+}
+
+ asyncF();
 
 let mapBox = () => {
   mapboxgl.accessToken = 'pk.eyJ1IjoiYWRyaWFuY3JyIiwiYSI6ImNrbnpxNzFtODA4NjIyb3FzMWpzcmc5ankifQ.RQ0oHHVzReNtVz678F61Ag';
   var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [2.19, 41.41],
-  zoom: 9
+  center: [ geolongitude, geolatitude],
+  zoom: 3
   });
    
   // Create a default Marker and add it to the map.
